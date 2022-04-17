deef5d45207193381d9de396b37fff0557b62813
Adarsh2k21 <adarsh.patel9619@gmail.com>
2022-04-18 04:40:01 +0530

    FIXED: styling, focus container

--- src/pages/homePage/HomePage.jsx
+++ src/pages/homePage/HomePage.jsx
@@ -1,6 +1,7 @@
 
 import { React, useState} from 'react'
 import { Clock, Quote} from '../../components'
+import { useNavigate } from 'react-router-dom'
 // import Quote from '../../components/quoteComponent/Quote'
 
 function HomePage() {
@@ -9,16 +10,18 @@ function HomePage() {
     const [ display, setDisplay ] = useState("block")
     const [ display1, setDisplay1 ] = useState("none")
 
-   
+    const navigate = useNavigate();
+    const myfocus = localStorage.getItem('focus');
     function keyHandler(e){
         if(e.key === 'Enter'){
             localStorage.setItem('focus', focus)
             setDisplay("none")
             setDisplay1("flex")
+            navigate('/main')
         }
     }
   return (
-    <div className='main-container'>
+    <div className='main-container home'>
     
         <Clock/>
         <h1 className='heading'>Good Evening, {name}.</h1>
@@ -28,13 +31,9 @@ function HomePage() {
         <input 
         onKeyPress={keyHandler} 
         onChange={(e) => setFocus(e.target.value)}
-        type="text" className='input-container-focus' placeholder=""/>
+        type="text" className='input-container-focus' placeholder="" required/>
         </div>
 
-        <div className='focus-container' style={{display:display1}}>
-        <input type="checkbox" className='checkbox' placeholder=""/>
-        <p className='para-focus'> {localStorage.getItem('focus')}</p>
-        </div>
         <Quote/>
     </div>
   )
--- src/pages/mainPage/MainPage.jsx
+++ src/pages/mainPage/MainPage.jsx
@@ -1,17 +1,36 @@
 
-import React from 'react'
-import { Clock } from '../../components'
+import {React, useState} from 'react'
+import { Clock,Quote } from '../../components'
 
 function MainPage() {
+  const [ style, setStyle ] = useState('none')
+  const [ check, setCheck ] = useState(true)
+
+  function focusHandler(e){
+    if(check){
+      setCheck(false)
+      setStyle('line-through')
+      console.log(check)
+    } else {
+      setStyle('none')
+      setCheck(true)
+    }
+
+  }
     const myfocus = localStorage.getItem('focus');
   return (
-    <div>
+    <div className='main-container main'>
         <Clock/>
-        <h1>Good Evening, {localStorage.getItem('userName')}.</h1>
-        <div>
-            <input type="checkbox" className='input-container' placeholder=""/>
-            <p>{myfocus}</p>
+        <h1 className='heading'>Good Evening, {localStorage.getItem('userName')}.</h1>
+       
+        <div className='focus-container'>
+            <input 
+            onChange={focusHandler}
+            value={check}
+            type="checkbox" className='checkbox' placeholder=""/>
+            <p className='para-focus' style={{textDecorationLine: style }}>{myfocus}</p>
         </div>
+        <Quote/>
     </div>
   )
 }
