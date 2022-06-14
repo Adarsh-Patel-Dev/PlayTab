273c19bb4aec1e431b7b9fbf9724efe1c8349f76
Adarsh2k21 <adarsh.patel9619@gmail.com>
2022-06-14 06:29:27 +0530

    refactor: clocck

--- src/components/clock/Clock.jsx
+++ src/components/clock/Clock.jsx
@@ -1,14 +1,13 @@
-import { React } from "react";
+import { React,useEffect, useState } from "react";
 
 function Clock() {
-  const updatedTime = () => {
-    return new Date().toLocaleTimeString(navigator.language, {
-      hour: "2-digit",
-      minute: "2-digit",
-    });
-  };
-  setInterval(updatedTime, 1000);
-  const time = updatedTime();
+  const [time, setTime ] = useState(new Date())
+
+  useEffect(() => {
+    let TimeId = setInterval(()=>setTime(new Date()),1000) 
+    return ()=> clearInterval(TimeId)
+  })
+  
   const hour = new Date().getHours();
   const greeting =
     "Good " + (hour < 12 ? "Morning" : hour < 18 ? "Afternoon" : "Evening");
@@ -16,7 +15,10 @@ function Clock() {
 
   return (
     <div className="time-container">
-      {time}
+      {time.toLocaleTimeString(navigator.language, {
+      hour: "2-digit",
+      minute: "2-digit",
+    })}
       <h1 className="heading">
         {greeting}, {user}.
       </h1>
