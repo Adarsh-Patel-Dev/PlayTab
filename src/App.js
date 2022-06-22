c50c03523f9f9a8625d4b617879f9bfedb7c3ec3
Adarsh2k21 <adarsh.patel9619@gmail.com>
2022-06-22 12:04:40 +0530

    FIX: image ratio, added dark screen

--- src/App.js
+++ src/App.js
@@ -6,14 +6,16 @@ import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
      <div className='bgImage'>
+    <div className="screen">
+    <div className='App'>
       <Weather/>
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="home" element={<HomePage />} />
         <Route path="/main" element={<MainPage />} />
       </Routes>
+      </div>
       </div>
     </div>
   );
